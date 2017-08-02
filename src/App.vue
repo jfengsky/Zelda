27b117f6e27aaa1e428de3613e284017711fd003
jfengsky <jfengsky@gmail.com>
2017-08-02 16:12:46 +0800

    add vue demo

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="example">
+        <h1>{{ msg }}</h1>
+        <ul>
+            <li v-for="n in 5">{{ n }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: 'Hello World!'
+        }
+    }
+}
+</script>
