261671f4dd531b0c0c944dd77921f827ab0914e0
jfengsky <jfengsky@gmail.com>
2017-08-02 17:45:32 +0800

    add vue demo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 /* 引入vue和主页 */
 import Vue from 'vue'
-import App from './App.vue'
 
-/* 实例化一个vue */
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#demo',
+  data: {
+    message: 'hello vue js'
+  }
 })
